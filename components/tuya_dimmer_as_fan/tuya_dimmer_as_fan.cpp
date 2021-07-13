168483a91843fbb43f0d9eff5a173e2e9a65513d
ChNussbaum <ChrisNussbaum@GMail.com>
2021-07-13 07:52:38 -0500

    Fefactor the Tuya dimmer as fan component

---
+++ components/tuya_dimmer_as_fan/tuya_dimmer_as_fan.cpp
@@ -0,0 +1,40 @@
+#include "esphome/core/log.h"
+#include "esphome/components/fan/fan_helpers.h"
+#include "tuya_dimmer_as_fan.h"
+
+namespace esphome {
+namespace tuya {
+
+static const char *const TAG = "tuya.fan";
+
+void TuyaDimmerAsFan::setup() {
+  auto traits = fan::FanTraits(false, false, false, 1);
+  this->fan_->set_traits(traits);
+
+  this->parent_->register_listener(*this->switch_id_, [this](const TuyaDatapoint &datapoint) {
+    auto call = this->fan_->make_call();
+    call.set_state(datapoint.value_bool);
+    call.perform();
+  });
+
+  this->parent_->register_listener(*this->dimmer_id_, [this](const TuyaDatapoint &datapoint) {
+    if (datapoint.value_int != this->dimmer_max_value_)
+    {
+      this->parent_->set_datapoint_value(*this->dimmer_id_, this->dimmer_max_value_);
+    }
+  });
+
+  this->fan_->add_on_state_callback([this]() { this->parent_->set_datapoint_value(*this->switch_id_, this->fan_->state); });
+
+  // Make sure we start at the max value
+  this->parent_->set_datapoint_value(*this->dimmer_id_, this->dimmer_max_value_);
+}
+
+void TuyaDimmerAsFan::dump_config() {
+  ESP_LOGCONFIG(TAG, "Tuya Dimmer as Fan:");
+  ESP_LOGCONFIG(TAG, "  Switch has datapoint ID %u", *this->switch_id_);
+  ESP_LOGCONFIG(TAG, "  Dimmer has datapoint ID %u", *this->dimmer_id_);
+}
+
+}  // namespace tuya
+}  // namespace esphome
