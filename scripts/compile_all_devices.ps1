de15281ec9806c3cc0107270d1df06a4ba705471
CHris Nussbaum <chrisnussbaum@gmail.com>
2022-04-28 20:53:50 -0500

    Finish removing underscores from names Move remaining devices to new wifi Compile all devices script does a backup first

--- scripts/compile_all_devices.ps1
+++ scripts/compile_all_devices.ps1
@@ -1,3 +1,17 @@
+Write-Output "`nBacking up build folder"
+if (Test-Path $PSScriptRoot\..\build\_backup)
+{
+    Remove-Item -Path $PSScriptRoot\..\build\_backup -Recurse -Force
+}
+if (Test-Path $PSScriptRoot\..\_backup)
+{
+    Remove-Item -Path $PSScriptRoot\..\_backup -Recurse -Force
+}
+New-Item $PSScriptRoot\..\_backup -ItemType Directory | Out-Null
+Move-Item -Path $PSScriptRoot\..\build\* -Destination $PSScriptRoot\..\_backup -Force
+Move-Item -Path $PSScriptRoot\..\_backup -Destination $PSScriptRoot\..\build -Force
+
+Write-Output "`nCompiling all devices"
 $files = Get-ChildItem $PSScriptRoot\..\devices\*.yaml -Exclude "secrets.yaml"
 $failures = New-Object Collections.Generic.List[String]
 foreach ($file in $files) {
